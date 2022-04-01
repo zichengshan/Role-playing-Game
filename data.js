0469e83a0e7b7318cb21f1b890dff6441f44b5c2
Zicheng.S <zichengshan@gmail.com>
2022-04-01 15:01:25 -0700

    import && export

---
+++ data.js
@@ -0,0 +1,18 @@
+const characterData = {
+    heroObject: {
+        elementId: "hero",
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: "60",
+        diceCount: 3
+    },
+
+    monsterObject: {
+        elementId: "monster",
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: "10",
+        diceCount: 1
+    }
+}
+export default characterData
--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const characterData = {
-    heroObject: {
-        elementId: "hero",
-        name: "Wizard",
-        avatar: "images/wizard.png",
-        health: "60",
-        diceCount: 3
-    },
-
-    monsterObject: {
-        elementId: "monster",
-        name: "Orc",
-        avatar: "images/orc.png",
-        health: "10",
-        diceCount: 1
-    }
-}
+import characterData from "./data.js";
+import {getDiceRollArray} from "./utils.js"
 
 function Character(data) {
     Object.assign(this, data)
@@ -39,12 +24,6 @@ function Character(data) {
             return `<div class="dice">${num}</div>`
         }).join('')
     }
-}
-
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function () {
-        return Math.floor(Math.random() * 6) + 1
-    })
 }
 
 function render() {
