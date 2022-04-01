1eb6915e28aaa4eb2ce85f8533a67c5d9dcbb673
Zicheng.S <zichengshan@gmail.com>
2022-04-01 11:13:01 -0700

    sway out for loop for map (map, join)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const heroObject = {
     name: "Wizard",
     avatar: "images/wizard.png",
     health: "60",
-    diceRoll: 6,
+    diceRoll: [3,1,4],
     diceCount: 3
 }
 
@@ -13,16 +13,15 @@ const monsterObject = {
     name: "Orc",
     avatar: "images/orc.png",
     health: "10",
-    diceRoll: 4,
+    diceRoll: [2],
     diceCount: 1
 }
 
 function renderCharacter(object) {
-    let diceHtml = ""
     const {elementId, name, avatar, health, diceRoll, diceCount} = object
-    for (let i = 0; i < diceCount; i++){
-        diceHtml += `<div class="dice">6</div>`
-    }
+    const diceHtml = diceRoll.map(function (roll){
+        return `<div class="dice">${roll}</div>`
+    }).join('')
 
     document.getElementById(elementId).innerHTML = `
         <div class="character-card">
