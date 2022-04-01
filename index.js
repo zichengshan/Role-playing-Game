87719770bc6344e4b214016137b501a66f8433d0
Zicheng.S <zichengshan@gmail.com>
2022-04-01 10:17:41 -0700

    render multiple dice
    Use a for loop to update diceHtml so that it contains the
    HTML for our dice. The number of dice needed is specificed in
    the diceCount property of the objects.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const heroObject = {
     name: "Wizard",
     avatar: "images/wizard.png",
     health: "60",
-    diceRoll: 6
+    diceRoll: 6,
+    diceCount: 3
 }
 
 const monsterObject = {
@@ -12,17 +13,25 @@ const monsterObject = {
     name: "Orc",
     avatar: "images/orc.png",
     health: "10",
-    diceRoll: 4
+    diceRoll: 4,
+    diceCount: 1
 }
 
 function renderCharacter(object) {
-    const {elementId, name, avatar, health, diceRoll} = object
+    let diceHtml = ""
+    const {elementId, name, avatar, health, diceRoll, diceCount} = object
+    for (let i = 0; i < diceCount; i++){
+        diceHtml += `<div class="dice">6</div>`
+    }
+
     document.getElementById(elementId).innerHTML = `
         <div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}"/>
             <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+            <div class="dice-container">
+                ${diceHtml}
+            </div>
         </div>
     `
 }
