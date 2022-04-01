6dc7dfadd89eb33e2cf81997c4ad52d446c95c58
Zicheng.S <zichengshan@gmail.com>
2022-04-01 14:30:50 -0700

    amalgamate the objects

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+const characterData = {
+    heroObject: {
+        elementId: "hero",
+        name: "Wizard",
+        avatar: "images/wizard.png",
+        health: "60",
+        diceCount: 3
+    },
 
-const heroObject = {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: "60",
-    diceCount: 3
+    monsterObject: {
+        elementId: "monster",
+        name: "Orc",
+        avatar: "images/orc.png",
+        health: "10",
+        diceCount: 1
+    }
 }
 
-const monsterObject = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: "10",
-    diceCount: 1
-}
-
-function Character(data){
+function Character(data) {
     Object.assign(this, data)
-    const { elementId, name, avatar, health, diceCount } = this;
-    this.getCharacterHtml = function (){
+    const {elementId, name, avatar, health, diceCount} = this;
+    this.getCharacterHtml = function () {
         const diceHtml = this.getDiceHtml(diceCount)
 
-         return `
+        return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}"/>
@@ -33,16 +34,16 @@ function Character(data){
     `
     }
 
-    this.getDiceHtml = function (diceCount){
-        return getDiceRollArray(diceCount).map(function (num){
+    this.getDiceHtml = function (diceCount) {
+        return getDiceRollArray(diceCount).map(function (num) {
             return `<div class="dice">${num}</div>`
         }).join('')
     }
 }
 
 function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function (){
-        return Math.floor(Math.random()*6)+1
+    return new Array(diceCount).fill(0).map(function () {
+        return Math.floor(Math.random() * 6) + 1
     })
 }
 
@@ -51,6 +52,6 @@ function render() {
     document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
 }
 
-const wizard = new Character(heroObject)
-const orc = new Character(monsterObject)
+const wizard = new Character(characterData.heroObject)
+const orc = new Character(characterData.monsterObject)
 render()
