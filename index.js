f2cb698aa975d214ca7c6657f6ed4ffbfab2ae57
Zicheng.S <zichengshan@gmail.com>
2022-04-01 21:06:32 -0700

    endGame add emoji

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ function endGame() {
         "No victors - all creatures are dead" :
         wizard.health > 0 ? "The Wizard Wins" :
         "The Orc is Victorious"
+
+    const endEmoji = endMessage === "The Wizard Wins" ? "🔮" : "☠️"
+    document.body.innerHTML = `
+    <div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+    </div>`
+
 }
 
 document.getElementById("attack-button").addEventListener("click", attack)
