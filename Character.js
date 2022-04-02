84f10079d634e6288f4977847f3cbdcf7f7e5ab9
Zicheng.S <zichengshan@gmail.com>
2022-04-01 22:18:25 -0700

    make the health bar work

--- Character.js
+++ Character.js
@@ -1,24 +1,29 @@
 import {getDiceRollArray, getDicePlaceholderHtml} from "./utils.js";
 
+const getPercentage = (remainingHealth, maximumHealth) => 100 * remainingHealth / maximumHealth
+
+const percent = getPercentage()
 function Character(data) {
     Object.assign(this, data)
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
-    this.maxHealth = data.health
+    this.maxHealth = this.health
 
     this.getCharacterHtml = function () {
         const {elementId, name, avatar, health, diceCount, diceArray} = this;
+        const healthBar = this.getHealthBarHtml()
         return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}"/>
                 <p class="health">health: <b> ${health} </b></p>
+                ${healthBar}
                 <div class="dice-container">
                     ${diceArray}
                 </div>
             </div>
     `
     }
-    
+
     // save data in diceArray
     this.getDiceHtml = function () {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
@@ -32,6 +37,18 @@ function Character(data) {
             this.health = 0
             this.dead = true
         }
+    }
+
+    this.getHealthBarHtml = function (){
+        const percent = getPercentage(this.health, this.maxHealth)
+        // alert(this.name + percent)
+        // const danger = percent <= 25 ? "danger" : ""
+        return `
+            <div class="health-bar-outer">
+                <div class="health-bar-inner ${percent <= 25 ? "danger" : ""} " 
+                    style="width: ${percent}%;">
+                </div>
+            </div>`
     }
 }
 
