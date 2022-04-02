e2ad2bebbd6bd0b7b48d499ccad5a898efe03f17
Zicheng.S <zichengshan@gmail.com>
2022-04-01 22:49:34 -0700

    Add new monster

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const characterData = {
-    heroObject: {
+    hero: {
         name: "Wizard",
         avatar: "images/wizard.png",
         health: "60",
@@ -7,12 +7,26 @@ const characterData = {
         currentDiceScore:[]
     },
 
-    monsterObject: {
+    orc: {
         name: "Orc",
         avatar: "images/orc.png",
         health: "100",
         diceCount: 1,
         currentDiceScore:[]
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.png",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
     }
 }
 export default characterData
--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import characterData from "./data.js";
 import Character from "./Character.js";
 
-function render() {
-    document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
-    document.getElementById("monster").innerHTML = orc.getCharacterHtml()
-}
+let monstersArray = ["orc", "demon", "goblin"];
 
-const wizard = new Character(characterData.heroObject)
-const orc = new Character(characterData.monsterObject)
-render()
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
 
 function attack() {
     wizard.getDiceHtml()
-    orc.getDiceHtml()
-    wizard.takeDamage(orc.currentDiceScore)
-    orc.takeDamage(wizard.currentDiceScore)
-    if(wizard.dead || orc.dead){
+    monster.getDiceHtml()
+    wizard.takeDamage(monster.currentDiceScore)
+    monster.takeDamage(wizard.currentDiceScore)
+    if(wizard.dead || monster.dead){
         endGame()
     }
     render()
 }
 
 function endGame() {
-    const endMessage = wizard.health === 0 && orc.health === 0 ?
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
         "No victors - all creatures are dead" :
         wizard.health > 0 ? "The Wizard Wins" :
         "The Orc is Victorious"
@@ -34,7 +32,14 @@ function endGame() {
         <h3>${endMessage}</h3>
         <p class="end-emoji">${endEmoji}</p>
     </div>`
-
 }
 
 document.getElementById("attack-button").addEventListener("click", attack)
+function render() {
+    document.getElementById("hero").innerHTML = wizard.getCharacterHtml()
+    document.getElementById("monster").innerHTML = monster.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.hero)
+let monster = getNewMonster()
+render()
